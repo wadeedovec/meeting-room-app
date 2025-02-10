e7c645bc97068779c0ce04eeba848ecd79df69ee
Wadee <wadeeaburayya@gmail.com>
2025-02-10 12:06:16 +0200

    deleted the mongo uri from db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the current directory path using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Load environment variables from the root .env file
+dotenv.config({ path: `${__dirname}/../../.env` });
 
 export const connectDB = async () => {
-    const MONGO_URI = "mongodb+srv://wadeeaburayya:kI1CmeH3rbGjSNvA@meeting-room-app.k0vm8.mongodb.net/meeting-room?retryWrites=true&w=majority&appName=meeting-room-app";
+    const MONGO_URI = process.env.MONGO_URI;
+
+    if (!MONGO_URI) {
+        console.error("Mongo URI not defined in .env file");
+        process.exit(1);
+    }
 
     try {
         const conn = await mongoose.connect(MONGO_URI);
         console.log(MONGO_URI);  // Log to check if the URI is correct
-
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
