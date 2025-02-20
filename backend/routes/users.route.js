edd175bcf0f678b336b6e022e2e13f09a41c2adb
Wadee <wadeeaburayya@gmail.com>
2025-02-20 11:49:00 +0200

    new ways for fetching

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getUsers, checkUser, createUser, updateUser, deleteUser } from "../controllers/user.controller.js";
 const router = express.Router();
-router.get("/", getUsers);
+//router.get("/", getUsers);
 router.post("/check-user", checkUser);
 router.post("/add-user", createUser);
 router.put("/:id", updateUser);
