046bb2482a6c30dac30b6c665b8efca572bda2d1
wadeedovec <wadee.aburayya@dovecgroup.com>
2025-04-02 17:12:50 +0300

    test

--- frontend/src/pages/TeamsLogin.jsx
+++ frontend/src/pages/TeamsLogin.jsx
@@ -13,15 +13,11 @@ function TeamsTabWithSSO() {
                     resources: ["api://booking.dovecgroup.com/" + import.meta.env.VITE_CLIENT_ID] // your exposed API scope
                 });
 
-                // Display the token in the UI for debugging (if needed)
-                // Remember to remove or protect sensitive information in production!
-                setData({ token });
-
-                const res = await fetch(`${import.meta.env.VITE_API_URI}token?token=${token}`);
-                const jsonData = await res.json();
-                setData(jsonData); // Update the data state with the fetched data
+                
+                setData(token);
             } catch (err) {
-                setError(`Could not get SSO token: ${err.message}`);
+                console.error("Error getting Teams auth token:", err);
+                setError("Could not get SSO token: " + err.message);
             }
         }
 
@@ -34,7 +30,6 @@ function TeamsTabWithSSO() {
     return (
         <div>
             <h1>Teams Tab with SSO</h1>
-            {/* Output data in a formatted way for debugging */}
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     );
