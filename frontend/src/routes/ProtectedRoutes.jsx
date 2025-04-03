b181660c8b7c3c9b27c21a9a8b5b3816f9937aff
wadeedovec <wadee.aburayya@dovecgroup.com>
2025-04-03 10:58:40 +0300

    protected route

--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
+import * as microsoftTeams from '@microsoft/teams-js';
 
 const ProtectedRoute = () => {
     const { user, loading } = useUser();
+    const [isTeamsEnvironment, setIsTeamsEnvironment] = useState(false);
+
+    useEffect(() => {
+        microsoftTeams.app.initialize();
+        microsoftTeams.app.getContext((context) => {
+            if (context) {
+                setIsTeamsEnvironment(true);
+            } else {
+                setIsTeamsEnvironment(false);
+            }
+        });
+    }, []);
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    return user ? <Outlet /> : <Navigate to="/login" replace />;
+    // Redirect logic based on the environment
+    if (!user) {
+        if (isTeamsEnvironment) {
+            // Redirect to Teams specific login if it's inside Microsoft Teams
+            return <Navigate to="/teamslogin" replace />;
+        } else {
+            // Redirect to regular login if not inside Teams
+            return <Navigate to="/login" replace />;
+        }
+    }
+
+    // Render children routes if user is authenticated
+    return <Outlet />;
 };
 
 export default ProtectedRoute;
